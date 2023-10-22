1644ed5b78bec389ac4348370d04ea1445ea8b63
Simon Diaz <simon.diaz.abad@gmail.com>
2023-10-21 20:42:35 -0400

    initialized cdk test stack

--- lib/serverless-crud-test-stack.ts
+++ lib/serverless-crud-test-stack.ts
@@ -1,16 +1,56 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+  Stack,
+  StackProps,
+  aws_lambda as lambda,
+  aws_dynamodb as dynamodb,
+  aws_apigateway as apigateway,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class ServerlessCrudTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface EvnStackProps extends StackProps {
+  prod: boolean;
+}
+
+export class ServerlessCrudTestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: EvnStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    let tableName, lambdaConst, apiGatewayName, dynamodbReadWrite, concurrency;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessCrudTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if (props?.prod) {
+      dynamodbReadWrite = 200;
+      concurrency = 100;
+      tableName = "prod-serverless-crud";
+      lambdaConst = { TABLE_NAME: tableName };
+      apiGatewayName = "prod-api-gw";
+    } else {
+      dynamodbReadWrite = 5;
+      concurrency = 5;
+      tableName = "staging-serverless-crud";
+      apiGatewayName = "staging-api-gw";
+    }
+
+    const welcomeLambda = new lambda.Function(this, "helloHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      environment: lambdaConst,
+      handler: "hello.handler",
+    });
+
+    const api = new apigateway.RestApi(this, apiGatewayName, {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const apiHelloIntg = new apigateway.LambdaIntegration(welcomeLambda);
+
+    const apiHello = api.root.addResource("hello");
+
+    apiHello.addMethod("GET", apiHelloIntg, {
+      authorizationType: apigateway.AuthorizationType.NONE,
+    });
   }
 }
